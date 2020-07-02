5c20825752b1e2c748705c6228b054d58318e937
Matheus Botelho <matheusbc97@users.noreply.github.com>
2020-07-02 13:15:15 -0300

    Tratativa de erros

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -19,22 +19,18 @@ appointmentRouter.get('/', async (req, res) => {
 });
 
 appointmentRouter.post('/', async (req, res) => {
-  try {
-    const { provider_id, date } = req.body;
+  const { provider_id, date } = req.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentServer();
+  const createAppointment = new CreateAppointmentServer();
 
-    const appointment = await createAppointment.execute({
-      date: parsedDate,
-      provider_id,
-    });
+  const appointment = await createAppointment.execute({
+    date: parsedDate,
+    provider_id,
+  });
 
-    return res.json(appointment);
-  } catch (error) {
-    return res.status(400).json({ error: error.message });
-  }
+  return res.json(appointment);
 });
 
 export default appointmentRouter;
